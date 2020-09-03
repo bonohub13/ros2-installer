fef900cbb2d1f6ea426fbb8dc4cbe8cfa1b09abc
bonohub13 <ken.saito.0813@gmail.com>
2020-09-03 15:54:34 +0900

    added line to install colcon

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## ::::: Updates :::::
 - September 2, 2020
     - Initialized repository
+- September 3, 2020
+    - added ```python3-colcon-common-extensions``` to install to use colcon
--- ubuntu/setup.bash
+++ ubuntu/setup.bash
@@ -12,4 +12,4 @@ sudo apt update && sudo apt install curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo apt update
-sudo apt install ros-foxy-desktop
+sudo apt install ros-foxy-desktop python3-colcon-common-extensions
